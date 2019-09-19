516f65ec6f4af82359844c06c872ef9a81f5d1de
GaryEstGt <31348611+GaryEstGt@users.noreply.github.com>
2019-09-19 11:44:31 -0600

    laboratorio

---
+++ ej1.asm
@@ -0,0 +1,27 @@
+.MODEL small
+.DATA
+; variable que contiene el texto que queremos mostrar
+cadena DB 'Gary Andrez MoranN$'
+cadena2 DB '1062917$'
+cadena3 DB 'El simbolo ASCII es$'
+.STACK
+.CODE
+Programa: ;etiqueta de inicio de programa
+;inicializar programa
+MOV AX, @Data
+MOV DS, AX
+
+; imprimir cadena
+MOV DX,OFFSET cadena ; asignando a DX la variable cadena
+MOV AH, 09h          ; digo que se imprimira una cadena
+INT 21h              ;ejecuta la interrupcion, imprimira
+
+MOV DX,OFFSET cadena2 ; asignando a DX la variable cadena
+MOV AH, 09h          ; digo que se imprimira una cadena
+INT 21h              ;ejecuta la interrupcion, imprimira
+           
+
+;finalizar programa
+MOV AH, 4Ch
+INT 21h
+END Programa
